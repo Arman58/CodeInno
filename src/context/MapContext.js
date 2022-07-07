fd086dd1903ff6c9bea3f724fb69a6f97d863c3b
Arman58 <Shaqaryan99@bk.ru>
2022-07-07 16:25:20 +0400

    refresh components after submit new feedback

--- src/context/MapContext.js
+++ src/context/MapContext.js
@@ -9,17 +9,31 @@ export const MapProvider = ({children}) => {
         name: "Khinkali Restaurant",
         rate: 2.1538461538461537,
     })
+    const [feedback, setFeedback] = useState({})
 
+    const [currentValue, setCurrentValue] = useState(0);
+
+    const handleClick = value => {
+        setCurrentValue(value)
+    }
+
+    const submitting = (id, body) => {
+        sendFeedback(id, body).then(r => setFeedback(r.data))
+    }
 
     const changeLocation = (id) => {
         getRestaurant(id).then(r => setLocation(r))
     }
 
 
-
     const value = {
         location,
         changeLocation,
+        submitting,
+        feedback,
+        currentValue,
+        setCurrentValue,
+        handleClick
     }
 
     return <MapContext.Provider value={value}>{children}</MapContext.Provider>
